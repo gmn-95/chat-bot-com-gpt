9fbcc383faf4ee5010bf80266c5a61cc8002f18a
Gustavo Macedo <gustavo.macedo9@outlook.com>
2023-05-06 15:30:51 -0300

    Refatorando controller

--- chat-bot/src/main/java/br/com/chatbot/controllers/ChatBotController.java
+++ chat-bot/src/main/java/br/com/chatbot/controllers/ChatBotController.java
@@ -3,10 +3,9 @@ package br.com.chatbot.controllers;
 
 import br.com.chatbot.service.ChatBotService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chat")
@@ -18,5 +17,17 @@ public class ChatBotController {
     @PostMapping("/pergunta")
     public Object respostaChat(@RequestBody Object pergunta){
         return chatBotService.apiChat((String) pergunta);
+    }
+
+    @PostMapping ("/tipoChat")
+    public ResponseEntity<?> alteraTipoChat(@RequestBody String tipoChat){
+        try {
+            chatBotService.setTipoChat(tipoChat);
+            chatBotService.setResetaConfig(true);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Alterado!");
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao trocar tipo do chat");
+        }
     }
 }
